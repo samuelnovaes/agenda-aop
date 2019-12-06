42aba8a6372a6bb83fd9886219ea36382a00424a
Samuel Novaes <samuel.novaes.96@gmail.com>
2019-12-05 21:53:45 -0300

    update

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,15 +1,25 @@
+const Contact = require('../models/contact')
+
 exports.create = async (name, phone) => {
-
+	const contact = new Contact({ name, phone })
+	await contact.save()
 }
 
-exports.update = async () => {
-
+exports.update = async (id, name, phone) => {
+	const contact = await Contact.findByPk(id)
+	contact.name = name
+	contact.phone = phone
+	await contact.save()
 }
 
-exports.delete = async () => {
-
+exports.delete = async (id) => {
+	const contact = await Contact.findByPk(id)
+	await contact.destroy()
 }
 
-exports.list = async () => {
-	
+exports.list = async (userId) => {
+	const contacts = await Contact.findAll({
+		where: { userId }
+	})
+	return contacts
 }
