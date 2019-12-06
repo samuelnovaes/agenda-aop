4a7384983d982225c06e1a3c00370ece9160f7ce
Samuel Novaes <samuel.novaes.96@gmail.com>
2019-12-06 09:11:27 -0300

    update

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
 const router = require('express').Router()
-const UserController = require('../controllers/user')
+const User = require('../controllers/user')
 
 //Cadastrar usuário
 router.post('/', async (req, res) => {
 	try {
-		await UserController.register(req.body.username, req.body.password)
+		await User.register(req.body.username, req.body.password)
 		res.end()
 	}
 	catch (error) {
@@ -15,7 +15,7 @@ router.post('/', async (req, res) => {
 //Login de usuário
 router.post('/login', async (req, res) => {
 	try {
-		const token = await UserController.login(req.body.username, req.body.password)
+		const token = await User.login(req.body.username, req.body.password)
 		res.json({ token: `Bearer ${token}` })
 	}
 	catch (error) {
